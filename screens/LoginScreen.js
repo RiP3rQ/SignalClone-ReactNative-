81ed0a8ab6e3df7794a27712318aa7ce1ba39fc7
RiP3rQ <rafalpompa2000@gmail.com>
2022-12-17 20:58:45 +0100

    Login screen - Done

---
+++ screens/LoginScreen.js
@@ -0,0 +1,61 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Button, Input, Image } from "@rneui/base";
+import { StatusBar } from "expo-status-bar";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = () => {};
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri: "https://www.citypng.com/public/uploads/small/51610315264dnueuotvjocc5ks3mq9scwr9nnp5kbujto76vhkoxiw8nzjtac17i9nonf7ixkvr1jlkzaro2z1rl09kstkmdaydsxawqhwdzg5y.png",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          keyboardType="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          keyboardType="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+
+      <Button containerStyle={styles.button} onPress={signIn} title="Login" />
+      <Button containerStyle={styles.button} type="clear" title="Register" />
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  inputContainer: {
+    width: 300,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+});
