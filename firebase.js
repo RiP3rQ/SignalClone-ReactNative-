c63e900d58d438106d512f3d3983a12b2b610353
RiP3rQ <rafalpompa2000@gmail.com>
2022-12-17 21:37:55 +0100

    Firebase setup

--- firebase.js
+++ firebase.js
@@ -1,3 +1,5 @@
+import * as firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyC84P5UX-X7qfKaoJktHNNiOJuwVwjxfLo",
   authDomain: "signal-clone-yt-56181.firebaseapp.com",
@@ -6,3 +8,15 @@ const firebaseConfig = {
   messagingSenderId: "480826236606",
   appId: "1:480826236606:web:7eac2893999ca2b4da88ef",
 };
+
+let app;
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
