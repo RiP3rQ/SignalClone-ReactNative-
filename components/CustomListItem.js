98aed8245471ed2bd68b4d6d300e496a5bd4e8f6
RiP3rQ <rafalpompa2000@gmail.com>
2022-12-21 18:42:56 +0100

    chats screen icons working properly

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,23 +1,39 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Avatar, ListItem } from "@rneui/base";
+import { db } from "../firebase";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 
 const CustomListItem = ({ id, chatName, enterChat }) => {
+  const [chatMessages, setChatMessages] = useState([]);
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "chats", id, "messages"),
+      orderBy("timestamp", "asc")
+    );
+    return onSnapshot(q, (snapshot) => {
+      setChatMessages(snapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
+
   return (
     <ListItem onPress={() => enterChat(id, chatName)} key={id} bottomDivider>
       <Avatar
         rounded
         source={{
-          uri: "https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png",
+          uri:
+            chatMessages[chatMessages.length - 1]?.photoURL ||
+            "https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png",
         }}
       />
       <ListItem.Content>
         <ListItem.Title style={{ fontWeight: "800" }}>
           {chatName}
         </ListItem.Title>
-
         <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
-          ABC
+          {chatMessages[chatMessages.length - 1]?.displayName}:{" "}
+          {chatMessages[chatMessages.length - 1]?.message}
         </ListItem.Subtitle>
       </ListItem.Content>
     </ListItem>
